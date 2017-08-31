2a3bc3bf7a9eafff341efe495320f8e073f899bc
Sheyin <silveryn@gmail.com>
2017-08-30 19:18:49 -0500

    Date calculation complete

--- quote.py
+++ quote.py
@@ -50,25 +50,40 @@ def recordQuote(author, text):
 # Older messages no longer have a timestamp (only date in MM/DD/YYYY format)
 def dateCalculation(quotedDate):
 	today = datetime.datetime.today()
-	print("Today's date: " + str(today.strftime('%Y-%m-%d')))
-	year = month = today.strftime('%Y')
-	month = today.strftime('%B')
-	day = today.strftime('%d')
-	print("Month: " + month + " Day: " + day + " Year: " + year)
-	print("Yesterday: " + month + " " + int(day)-1 + " " + year)
 
-	if "Yesterday" in quotedDate:
+	# Populate week calendar
+	todayWeekday = str(today.strftime('%A'))
+	weekDict = {}
+
+	for i in range(1, 8):
+		dayEntry = datetime.datetime.now() - datetime.timedelta(days=i)
+		dayEntryLong = str(dayEntry.strftime('%A'))
+		weekDict['Last ' + dayEntryLong] = str(dayEntry.strftime('%Y-%m-%d'))
+	yesterdayEntry = datetime.datetime.now() - datetime.timedelta(days=1)
+	weekDict['Yesterday'] = str(yesterdayEntry.strftime('%Y-%m-%d'))
+
+	if "Today" in quotedDate:
+		return str(today.strftime('%Y-%m-%d'))
+	
+
+	elif "Yesterday" in quotedDate:
 		# Return day - 1
-		pass
+		return weekDict['Yesterday']
 
 	# Automatically converts up to a week to "Last Tuesday", etc.
 	elif "Last" in quotedDate:
-		# Figure out today's day then calculate how long ago it was
-		week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-		for index, weekday in enumerate(week, start = 0):
-			if weekday in quotedDate:
-				weekIndex = index
-		print("weekIndex: " + weekIndex + " day: " + week[weekIndex])
+		# Now locate entry in dictionary
+		for key in weekDict:
+			if key in quotedDate:
+				return weekDict[key]
 
-	
+	else:
+		print("Some error occurred.  Invalid date?  Returning today's date.")
+		return str(today.strftime('%Y-%m-%d'))
+			
 	return
+
+if __name__ == "__main__":
+	print(dateCalculation("Yesterday at 4:50 PM"))
+	print(dateCalculation("Last Friday at 5:50 PM"))
+	print(dateCalculation("Last Wednesday at 5:50 PM"))
